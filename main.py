617545604c58ae8e65dd39b10e036dc42a1c7ea1
Luke Sylvester <145699572+lukesylvester99@users.noreply.github.com>
2025-01-16 16:40:53 -0800

    created df and began looking at data

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import numpy as np
-
 from matplotlib import pyplot as plt
 
+#Load data into dataframes and visualize structure
 sightings_df = pd.read_csv('observations.csv')
+species_df = pd.read_csv('species_info.csv')
 
 print(sightings_df.head())
+print(species_df.head())
+
+#Initally Explore Data
+
+print("number of distinct species in dataset: ", species_df.scientific_name.nunique())
+

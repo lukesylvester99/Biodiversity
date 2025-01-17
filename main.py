72e1d11846af8aa9e82a2cc1e1985b0e6cd12075
Luke Sylvester <145699572+lukesylvester99@users.noreply.github.com>
2025-01-17 10:06:36 -0800

    started analyzing the data

--- main.py
+++ main.py
@@ -11,5 +11,15 @@ print(species_df.head())
 
 #Initally Explore Data
 
-print("number of distinct species in dataset: ", species_df.scientific_name.nunique())
+print("number of distinct species in dataset: ", species_df.scientific_name.nunique()) #how many animals are in our data set 
+
+print("number of animal classifications: ", species_df.category.nunique()) #how many types of species are in our data set
+print("species diversity: ", species_df.category.unique())
+
+print(species_df.groupby('category').size()) # of the types of species, how many animals belong to each one
+
+print("number of conservation statuses: ", species_df.conservation_status.nunique()) #what are the conservation designations
+print("unique conservation statuses: ", species_df.conservation_status.unique()) 
+print(species_df.groupby('conservation_status').size()) #of those designations, what statuses are the biggest
+
 
